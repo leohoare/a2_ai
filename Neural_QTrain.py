eff1c0d8c5a643a3036947671b39d66fa374f534
Leo Hoare <leo.hoare@live.com>
2018-10-14 17:12:23 +1000

    it runs...

--- Neural_QTrain.py
+++ Neural_QTrain.py
@@ -32,30 +32,29 @@ action_in = tf.placeholder("float", [None, ACTION_DIM])
 target_in = tf.placeholder("float", [None])
 
 # TODO: Define Network Graph
-def network(state_in, state_dim, action_dim, hidden_nodes = [50,50]):
+def network(state_dim, action_dim, hidden_nodes = [50,50]):
 	w1 = tf.get_variable("w1", shape=[state_dim, hidden_nodes[0]])
-	b1 = tf.get_variable("b1", shape=[1, hidden_nodes[0]], initializer = tf.constant_initializer(0.0)) 
+	b1 = tf.get_variable("b1", shape=[1, hidden_nodes[0]], initializer = tf.constant_initializer(0.0))
 
 	w2 = tf.get_variable("w2", shape=[hidden_nodes[0], hidden_nodes[1]])
-	b2 = tf.get_variable("b2", shape=[1, hidden_nodes[1]], initializer = tf.constant_initializer(0.0)) 
+	b2 = tf.get_variable("b2", shape=[1, hidden_nodes[1]], initializer = tf.constant_initializer(0.0))
 
 	w3 = tf.get_variable("w3", shape=[hidden_nodes[1], action_dim])
-	b3 = tf.get_variable("b3", shape=[1, action_dim], initializer = tf.constant_initializer(0.0)) 
+	b3 = tf.get_variable("b3", shape=[1, action_dim], initializer = tf.constant_initializer(0.0))
 
 	l1_logits = tf.matmul(state_in, w1) + b1
 	l1_out = tf.tanh(l1_logits)
-	# option for tf.sigmoid
-	
+
 	l2_logits = tf.matmul(l1_logits, w2) + b2
 	l2_out = tf.tanh(l2_logits)
 
 	l3_logits = tf.matmul(l2_logits, w3) + b3
 	l3_out = tf.tanh(l3_logits)
-	
+
 	return l3_out
 
 # TODO: Network outputs
-q_values = network(state_in, STATE_DIM, ACTION_DIM, [50,50])
+q_values = network(STATE_DIM, ACTION_DIM, [50,50])
 q_action = tf.reduce_sum(tf.multiply(q_values, action_in), reduction_indices=1)
 
 # TODO: Loss/Optimizer Definition
@@ -89,57 +88,58 @@ def explore(state, epsilon):
 # Main learning loop
 for episode in range(EPISODE):
 
-    # initialize task
-    state = env.reset()
-
+	# initialize task
+	state = env.reset()
     # Update epsilon once per episode
-    epsilon -= epsilon / EPSILON_DECAY_STEPS
+	epsilon -= epsilon / EPSILON_DECAY_STEPS
+	# Move through env according to e-greedy policy
 
-    # Move through env according to e-greedy policy
-    for step in range(STEP):
-        action = explore(state, epsilon)
-        next_state, reward, done, _ = env.step(np.argmax(action))
 
-        nextstate_q_values = q_values.eval(feed_dict={
-            state_in: [next_state]
-        })
+	for step in range(STEP):
+		action = explore(state, epsilon)
+		next_state, reward, done, _ = env.step(np.argmax(action))
+		#print(next_state.shape[0])
+		# next_state = next_state.reshape([1,next_state.shape[0]])
+		nextstate_q_values = q_values.eval(feed_dict={
+			state_in: [next_state]
+		})
 
-	
-	
-        # TODO: Calculate the target q-value.
-        # hint1: Bellman
-        # hint2: consider if the episode has terminated
-        # target =
-	# NEED TO DEFINE TARGET
-        # Do one training step
-        session.run([optimizer], feed_dict={
-            target_in: [target],
-            action_in: [action],
-            state_in: [state]
-        })
-
-        # Update
-        state = next_state
-        if done:
-            break
+		#print(f" reward = {reward} next_state_q = {nextstate_q_values}")
+		target = reward + GAMMA * nextstate_q_values[0]
+		##### comment out when submitting #####
+		#env.render()
+		#######################################
+		# TODO: Calculate the target q-value.
+		# hint1: Bellman
+		# hint2: consider if the episode has terminated
+		# target =
+		# NEED TO DEFINE TARGET
+		# Do one training step
+		session.run([optimizer], feed_dict={ state_in: [state],
+			target_in: target, action_in: [action]
+		})
+		# Update
+		state = next_state
+		if done:
+			break
 
     # Test and view sample runs - can disable render to save time
     # -- DO NOT MODIFY --
-    if (episode % TEST_FREQUENCY == 0 and episode != 0):
-        total_reward = 0
-        for i in range(TEST):
-            state = env.reset()
-            for j in range(STEP):
-                env.render()
-                action = np.argmax(q_values.eval(feed_dict={
-                    state_in: [state]
-                }))
-                state, reward, done, _ = env.step(action)
-                total_reward += reward
-                if done:
-                    break
-        ave_reward = total_reward / TEST
-        print('episode:', episode, 'epsilon:', epsilon, 'Evaluation '
+	if (episode % TEST_FREQUENCY == 0 and episode != 0):
+		total_reward = 0
+		for i in range(TEST):
+			state = env.reset()
+			for j in range(STEP):
+				env.render()
+				action = np.argmax(q_values.eval(feed_dict={
+					state_in: [state]
+				}))
+				state, reward, done, _ = env.step(action)
+				total_reward += reward
+				if done:
+					break
+				ave_reward = total_reward / TEST
+				print('episode:', episode, 'epsilon:', epsilon, 'Evaluation '
                                                         'Average Reward:', ave_reward)
 
 env.close()
